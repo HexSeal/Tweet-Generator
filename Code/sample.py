8b7379d97b3e3b40b27863c438111cb002262b62
HexSeal <maxfin13@gmail.com>
2019-11-04 22:38:12 -0800

    Fixing sample.py

--- Code/sample.py
+++ Code/sample.py
@@ -2,22 +2,27 @@ import histogram
 import sys
 import random
 
+# Helper function for getting the total number of objects in the histogram
+get_total(histogram):
+
 
 # Take the token(total number of objects) and divide the frequency of the words by tokens to get a decimal. Then use that to select a random word based on frequency.
-
 def sample_by_frequency(histogram):
-    token = sum(histogram.values)
+    # Get the "weight" of every letter together
+    token = len(histogram)
+    # Get the lucky index
+    random_index = random.randint(1, token)
 
-    random_index = random.randint()
-
+    # Iterate through all the words 
     for word in histogram:
-        if word == histogram[random_index]:
+        # Subtract the words value from the histogram
+        random_index = random_index - word.values
+        if random_index <= 0:
             return word
+            
 
-def word_percentage(histogram, word):
-    histo_len = len(histogram)
-    word_freq = histogram.frequency(word, histogram)
-    word_percentage = (word_freq/histo_len) * 100
-    return word_percentage
 
 if __name__ == "__main__":
+    source_text = 'harry_potterb1.txt'
+    histogram = histogram.histogram_dictionary(source_text)
+    sample_by_frequency(histogram)
