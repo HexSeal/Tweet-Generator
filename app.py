8568c9e4c85f29507fde238754609921ffdb7c92
HexSeal <maxfin13@gmail.com>
2019-12-11 20:14:45 -0800

    Changes made for heroku deployment

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
+from higher_order_markov import SecondOrderChain
 
-from dictogram import Dictogram
 import os
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Tweet-Generator')
@@ -10,10 +10,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """ Homepage """
-    word_doc = 'harry_potterb1.txt'
-    words = run(word_doc)
-    histogram = Dictogram(words)
-    sentence = histogram.get_sentence(15)
+    word_list = SecondOrderChain.get_text("jordanpeterson.txt")
+    markov_chain = SecondOrderChain(word_list)
+    sentence = markov_chain.word_walk(8)
+
     return render_template("index.html", tweet=sentence)
 
 if __name__ == "__main__":
